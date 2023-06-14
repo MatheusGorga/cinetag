3cdc6d508866923fdc7b50e03a4a7ac78d388896
MatheusGorga <matheusgobar@gmail.com>
2023-06-13 22:43:31 -0300

    up

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,13 +1,16 @@
 import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
 import Banner from "components/Banner";
-
+import Titulo from "components/Titulo";
 
 function Inicio() {
   return (
     <>
       <Cabecalho />
-        <Banner imagem="home"/>
+      <Banner imagem="home" />
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+      </Titulo>
       <Rodape />
     </>
   );
